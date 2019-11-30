eb5b627f56b4ff7189863d27739428e14a446cac
Dat Le <lhdat@apcs.vn>
2019-11-30 20:44:48 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is main page</Text>
+      </View>
+    );
+  }
+}
