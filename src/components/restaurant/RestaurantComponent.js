589d9fbba0b301a4128f859453a098af4c6e403a
DatLe-99 <lhdat@apcs.vn>
2019-12-27 16:32:12 +0700

    Fix-get-data-bug

--- src/components/restaurant/RestaurantComponent.js
+++ src/components/restaurant/RestaurantComponent.js
@@ -19,13 +19,25 @@ import {
 export default class RestaurantComponent extends Component{
     constructor(props){
         super(props)
-        this.data = this.props.navigation.getParam('listMenu')
+        // this.data = this.props.navigation.getParam('listMenu')
+         /**Sai nè nha, trong constructor phải khai báo state, sửa như ở dưới*/
+         this.state = {
+             listData : this.props.navigation.getParam('listMenu')
+         }
+    }
+    componentDidMount = () => {
+        console.log(this.state.listData.item)
     }
     render(){
         return(
             <View>
-                <Text>{this.data.Categories[0].Items[0].name}</Text>
-                {/* Can not get this data */}
+                {/* <Text>{this.data.Categories[0].Items[0].name}</Text> */} 
+                {/* Can not get this data => Chỗ này lấy data không cần phức tạp vậy =.=
+                cái item được truyền ở trong flatlist nó đã là 1 object rồi, lấy đơn giản như ở dưới đây là được!!! 
+                */}
+            <Text>{this.state.listData.item.name}</Text>
+
+
             </View>
         );
     }
